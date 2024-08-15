cf7b573b2e51ce9706e07f6ecd6538ce14470459
Sofia Parreira <sofia.parreira13@gmail.com>
2024-08-14 21:28:24 -0300

    exercicio6.js

---
+++ aula03-condicional/exercicio6.js
@@ -0,0 +1,9 @@
+const JORNADA = 40;
+const HORAST = 48;
+const HORASFEITAS = (HORAST - JORNADA);
+
+    if (HORASFEITAS > JORNADA) {
+        console.log(`O funcionário trabalhou ${HORASFEITAS} horas extras.`);
+    } else if (HORAST === JORNANDA) {
+        console.log("O funcionário trabalhou sua jornada de 40 horas.");
+    }
